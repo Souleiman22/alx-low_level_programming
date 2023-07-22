b070ee9975a72b53d55e44f9cbb76fe872cb4219
Souleiman22 <souleimangeudi@gmail.com>
2023-07-22 03:07:39 -0700

    changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
+ * Description: Can only use _putchar twice.
  */
 void print_most_numbers(void)
 {
@@ -15,4 +16,6 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
+
+	_putchar('\n');
 }
