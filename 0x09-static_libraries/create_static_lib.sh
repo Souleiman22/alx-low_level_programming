360774e15df6ec2aa9677a0d6cd2f43afef5c568
Souleiman22 <Souleimangeudi@gmail.com>
2023-08-06 09:14:04 -0700

    static_libraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+# Collect all the .c files in the current directory
+c_files=$(ls *.c 2>/dev/null)
+
+# Check if there are any .c files
+if [ -z "$c_files" ]; then
+    echo "No .c files found in the current directory."
+    exit 1
+fi
+
+# Compile each .c file into object files
+object_files=""
+for file in $c_files; do
+    object_file="${file%.c}.o"
+    gcc -c "$file" -o "$object_file"
+    object_files+=" $object_file"
+done
+
+# Create the static library (liball.a) from the object files
+ar rcs liball.a $object_files
+
+# Cleanup: remove the temporary object files
+rm -f $object_files
+
+echo "Static library liball.a successfully created."
+
